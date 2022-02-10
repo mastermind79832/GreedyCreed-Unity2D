2b0cf0894c10aab0d030a1dff3f240c803eec4d3
Aditya E Ajith <adityaeajith@gmail.com>
2022-02-11 03:22:06 +0530

    Created TileMap
    Created TIleMap
    Created PlayerController

---
+++ Greedy Creed/Assets/_project/Scripts/PlayerContoller.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+    public string Username = "Anonymus";
+    public int healthCount = 3;
+    public float MoveSpped = 6f;
+}
